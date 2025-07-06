9adb1de9febce06e21dbdda61c890b6990d73876
Alex Nurci <alexnurci@gmail.com>
2025-07-05 20:33:13 -0400

    Fixing edit component

--- src/components/EditCampus.jsx
+++ src/components/EditCampus.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
+//Component not working, needs fixing
+
 const EditCampus = () => {
   const { campusId } = useParams();
 
