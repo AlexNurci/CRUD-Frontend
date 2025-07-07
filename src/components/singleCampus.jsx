fbe9b2cf9cfdaf2abd235236f5b607a4bd2bac6e
Alex Nurci <alexnurci@gmail.com>
2025-07-07 00:52:35 -0400

    Imported css changes from jocsan branch

--- src/components/singleCampus.jsx
+++ src/components/singleCampus.jsx
@@ -29,49 +29,49 @@ const SingleCampus = () => {
   if (loading) return <p>Loading student data...</p>;
   if (!campus) return <p>Loading campus...</p>;
 
-  return (
-    <div className="single-campus">
-      <br></br>
-      <h2>{campus.campusName}</h2>
-      <br></br>
-      <p>Location: {campus.address}</p>
-      <br></br>
-      <p>Abouts us: {campus.description}</p>
-      <br></br>
-      <br></br>
-      <h3>Enrolled Students</h3>
-      {campus.students?.length ? (
-        <ul className="rem-list">
-          {campus.students.map((s) => (
-            <li key={s.id}>
-              <NavLink to={`/students/${s.id}`}>
-                {s.firstName} {s.lastName}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No students enrolled.</p>
-      )}
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-        <Routes>
-          <Route path={`/campuses/${campus.id}/edit`} element={<EditCampus />}/>
-        </Routes>
-        <br></br>
-        <NavLink className="editButton" to={`/campuses/${campus.id}/edit`}>Edit Campus</NavLink>
+ return (
+  <div className="single-campus">
+    <h2 className="h2campus">{campus.campusName}</h2>
+
+    <div className="box">
+      <div className="content-box"> 
+        <p>Location: {campus.address}</p>
+        <p>{campus.students?.length || 0} students</p>
+
+        <div className="description-box">
+          <p>About us: {campus.description}</p>
+        </div>
+      </div>
+
+      <div className="image-box">
+        {campus.image && (
+          <img src={campus.image} alt={`${campus.campusName} campus`} />
+        )}
+      </div>
     </div>
-  );
-};
+
+    <h3 className="h2css">Enrolled Students</h3>
+    {campus.students?.length ? (
+      <ul className="rem-list">
+        {campus.students.map((s) => (
+          <li key={s.id}>
+            <NavLink to={`/students/${s.id}`}>
+              {s.firstName} {s.lastName}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p>No students enrolled.</p>
+    )}
+
+    <div className="button-group">
+    <NavLink className="editButton" to={`/campuses/${campus.id}/edit`}>
+      Edit Campus
+    </NavLink>
+    </div>
+  </div>
+);
+}
 
 export default SingleCampus;
