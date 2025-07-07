679c1aaff73254671c3f787851751124523455f0
Alex Nurci <alexnurci@gmail.com>
2025-07-07 01:07:18 -0400

    Changed home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
 //Most likely changing the home page
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Website where you can add and remove campuses and students!</h1>  
+      <h1 style={{ textAlign: "center" }}>Campus and student view and edit website!</h1>  
       <h2 style={{ textAlign: "center" }}>You can view everything using the Navbar!</h2>
     </div>
   );
